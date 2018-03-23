dc788f070215889e667349123005ca886690dc9c
Ming Sun <sun_ming@bsi.com>
2018-03-23 14:08:38 -0400

    changed to to dao here 1-3 in App,java

--- src/app/App.java
+++ src/app/App.java
@@ -16,7 +16,7 @@ public class App {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-	// to do here 1-2
+	// to do here 1-3
 	// to do here 2
 	// to do here 3
 
